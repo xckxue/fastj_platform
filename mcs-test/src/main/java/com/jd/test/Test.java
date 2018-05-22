eb53c89510d2a48a2feca5c97be7af5c1f902682
xckxue@126.com <xckxue@126.com>
2018-05-22 11:29:38 +0800

    20180522version2

--- mcs-test/src/main/java/com/jd/test/Test.java
+++ mcs-test/src/main/java/com/jd/test/Test.java
@@ -5,6 +5,6 @@ package com.jd.test;
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("version 1");
+        System.out.println("version 2");
     }
 }
